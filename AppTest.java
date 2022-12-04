d4ab2a2ac6471238859498ed3909aaa4c8febb87
jilan <jilan@gmail.com>
2022-12-04 17:57:54 +0530

    added file

---
+++ AppTest.java
@@ -0,0 +1,28 @@
+package com.Selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+        
+
+    }
+}
